2b545a20c68033cb0b819be85fe35bbb270e23b1
AndersenCastaneda <andersenpiano@gmail.com>
2020-08-15 05:03:40 +0000

    Fix check 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,26 +8,18 @@ package { 'nginx':
   require => Exec['apt-get-update'],
 }
 
+file_line { 'custom_header':
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'server_name _;',
+  line    => 'add_header X-Served-By $hostname;'
+  require => Package['nginx'],
+}
+
 file_line { 'redirection':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default'
   after   => 'server_name _;',
   line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  require => Package['nginx'],
-}
-
-file_line { 'not_found':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default'
-  after   => 'server_name _;',
-  line    => 'error_page 404 /custom_404.html;',
-  require => Package['nginx'],
-}
-
-file_line { 'custom_header':
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'server_name _;',
-  line    => 'add_header X-Served-By $hostname;'
   require => Package['nginx'],
 }
 
