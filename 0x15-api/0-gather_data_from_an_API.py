2aee90714bb5b8aaac712e3232d766884d4426fe
AndersenCastaneda <andersenpiano@gmail.com>
2020-09-14 21:54:51 +0000

    Use .get()

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
         uid = sys.argv[1]
         u = 'https://jsonplaceholder.typicode.com/users/{}'.format(uid)
         t = 'https://jsonplaceholder.typicode.com/todos/?userId={}'.format(uid)
-        name = requests.get(u).json()['name']
+        name = requests.get(u).json().get('name')
         rq_todo = requests.get(t).json()
-        tasks = [i['title'] for i in rq_todo if i['completed'] is True]
+        tasks = [i['title'] for i in rq_todo if i.get('completed') is True]
         print('Employee {} is done with tasks({}/{}):'
               .format(name, len(tasks), len(rq_todo)))
         print('\n'.join('\t{}'.format(i) for i in tasks))
