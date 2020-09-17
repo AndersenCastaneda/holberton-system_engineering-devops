2ebdb6a43fa047936101d51d5279665d44f7771b
AndersenCastaneda <andersenpiano@gmail.com>
2020-09-17 13:18:51 -0500

    Add task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+1-top_ten
+"""
+
+
+def top_ten(subreddit):
+    """Queries the Reddit API and returns the number of subscribers"""
+    import requests
+
+    # url = 'https://www.reddit.com/r/{}/hot.json/'.format(subreddit)
+    url = 'https://www.reddit.com/r/programming/hot.json'
+    user_agent = {'User-Agent': 'Chrome/85.0.4183.102'}
+    request = requests.get(url, headers=user_agent)
+    if request.status_code == 200:
+        subscribers = request.json().get('data').get('children')
+        count = 0
+        for hot in subscribers:
+            if count == 10:
+                break
+            count += 1
+            print(hot.get('data').get('title'))
+    else:
+        print(None)
