5df9385b665ec6a0bdf71f13093a8140b90c6e53
AndersenCastaneda <andersenpiano@gmail.com>
2020-09-17 11:06:56 -0500

    Add task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+0-subs
+"""
+
+
+def number_of_subscribers(subreddit):
+    """Queries the Reddit API and returns the number of subscribers"""
+    import requests
+    import json
+
+    if subreddit is None or type(subreddit) != str:
+        return 0
+
+    url = 'https://www.reddit.com/r/programming/about.json'
+    user_agent = {'User-agent': 'Chrome/85.0.4183.102'}
+    request = requests.get(url, headers=user_agent)
+    subscribers = request.json().get('data').get('subscribers')
+    return (subscribers)
+
+if __name__ == "__main__":
+    pass
