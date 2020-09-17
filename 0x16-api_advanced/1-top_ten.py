e2d237126a77dbd4c7bcf9fc50e0f4eb4e5ac39e
AndersenCastaneda <andersenpiano@gmail.com>
2020-09-17 13:19:39 -0500

    Fix requirement

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,8 +8,7 @@ def top_ten(subreddit):
     """Queries the Reddit API and returns the number of subscribers"""
     import requests
 
-    # url = 'https://www.reddit.com/r/{}/hot.json/'.format(subreddit)
-    url = 'https://www.reddit.com/r/programming/hot.json'
+    url = 'https://www.reddit.com/r/{}/hot.json/'.format(subreddit)
     user_agent = {'User-Agent': 'Chrome/85.0.4183.102'}
     request = requests.get(url, headers=user_agent)
     if request.status_code == 200:
