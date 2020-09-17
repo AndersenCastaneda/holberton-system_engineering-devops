5685805447c0156f4e2f7a22caffb062836ebccb
AndersenCastaneda <andersenpiano@gmail.com>
2020-09-17 12:25:40 -0500

    Fix requirement

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,14 +8,11 @@ def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers"""
     import requests
 
-    url = 'https://www.reddit.com/r/programming/about.json'
-    user_agent = {'User-agent': 'Chrome/85.0.4183.102'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    user_agent = {'User-Agent': 'Chrome/85.0.4183.102'}
     request = requests.get(url, headers=user_agent)
     if request.status_code == 200:
         subscribers = request.json().get('data').get('subscribers')
-        return (subscribers)
+        return subscribers
 
     return 0
-
-if __name__ == "__main__":
-    pass
