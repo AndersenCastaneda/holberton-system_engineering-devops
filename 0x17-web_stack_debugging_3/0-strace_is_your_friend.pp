792906f27d3315cdeded4742f5f5443091c9e18a
AndersenCastaneda <andersenpiano@gmail.com>
2020-09-29 18:58:35 -0500

    Fix puppet style

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 exec { 'Fix 500 error':
-	onlyif  => 'test -e /var/www/html/wp-settings.php',
-	command => 'sed -i "s/phpp/php" /var/www/html/wp-settings.php',
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => 'sed -i "s/phpp/php" /var/www/html/wp-settings.php',
 }
