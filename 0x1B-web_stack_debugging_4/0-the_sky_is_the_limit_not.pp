bc8712afc464898ccc1680a5d7fff10ae44c4ae0
AndersenCastaneda <andersenpiano@gmail.com>
2020-10-08 09:48:45 -0500

    Fix output case 4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,6 @@
 # Fix ulimit
 exec { 'Fix ulimit':
   onlyif  => 'test -e /etc/default/nginx',
-  command => "sed -i 's/15/4096/' /etc/default/nginx",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
-}
-
-exec { 'Restart nginx':
-  command => 'sudo service nginx restart',
+  command => "sed -i 's/15/4096/' /etc/default/nginx && sudo service nginx restart",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 }
