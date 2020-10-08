280f95c2f44a9b35358a3a2c0e91959155ec0bec
AndersenCastaneda <andersenpiano@gmail.com>
2020-10-08 18:02:43 -0500

    Add task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,5 @@
 # Fix ulimit
 exec { 'Change OS Configuration':
   command => "sed -i 's/holberton/andersen/' /etc/security/limits.conf",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 }
